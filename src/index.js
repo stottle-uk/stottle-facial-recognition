70ddbf9d1d462d8e904ca8ebe56ba6a6b9ed2355
Stuart <stuarttottle@hotmail.com>
2017-11-20 20:40:43 +0000

    get image file file system

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-var rp = require('request-promise-native');
+const rp = require('request-promise-native');
+const fs = require('fs-extra');
+
+// function to encode file data to base64 encoded string
+const base64_encode = (file) => {
+    const bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
 
 const detectImage = async (optons) => {
     let response = await rp(optons);
-    console.log(response);
+    await fs.writeJson(__dirname + '\\result.txt', response, { spaces: 2 });
     return response;
 }
+
+const body = { image: base64_encode(__dirname + "\\images\\PIC00113.JPG"), selector: 'roll' };
 
 const rpOptions = {
     method: 'POST',
@@ -13,7 +22,7 @@ const rpOptions = {
         app_id: '83d919bc',
         app_key: '9c32a38c7738cc8f1c86e77b310d24bb'
     },
-    body: { image: 'https://media.kairos.com/liz.jpg', selector: 'roll' },
+    body: body,
     json: true
 };
 
