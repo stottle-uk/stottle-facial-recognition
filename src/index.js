be85b0c12e003bd043e7536f36318a374cef829e
Stuart <stuarttottle@hotmail.com>
2017-11-20 22:14:38 +0000

    tidy up

--- src/index.js
+++ src/index.js
@@ -2,28 +2,43 @@ const rp = require('request-promise-native');
 const fs = require('fs-extra');
 
 // function to encode file data to base64 encoded string
-const base64_encode = (file) => {
-    const bitmap = fs.readFileSync(file);
+const base64Encode = async (file) => {
+    const bitmap = await fs.readFile(file);
     return new Buffer(bitmap).toString('base64');
 }
 
-const detectImage = async (optons) => {
-    let response = await rp(optons);
-    await fs.writeJson(__dirname + '\\result.txt', response, { spaces: 2 });
-    return response;
-}
+const getAllFileNamesFromDirectory = async (path) => await fs.readdir(path);
 
-const body = { image: base64_encode(__dirname + "\\images\\PIC00113.JPG"), selector: 'roll' };
-
-const rpOptions = {
+const getKariosInfo = async (url, body) => await rp({
     method: 'POST',
-    uri: 'http://api.kairos.com/detect',
+    uri: url,
     headers: {
         app_id: '83d919bc',
         app_key: '9c32a38c7738cc8f1c86e77b310d24bb'
     },
     body: body,
     json: true
-};
+});
 
-let t = detectImage(rpOptions);
+const go = async (imagesBase) => {
+
+    const fileNames = await getAllFileNamesFromDirectory(imagesBase);
+
+    fileNames
+        .filter(p => p.split('.').pop().toLowerCase() === 'jpg')
+        .forEach(async filename => {
+            const imageData = await base64Encode(`${imagesBase}\\${filename}`);
+            const body = { image: imageData, subject_id: 'Stuart Tottle', gallery_name: "MyFirstGallery" };
+            const response = await getKariosInfo('https://api.kairos.com/enroll', body)
+            await fs.writeJson(`${imagesBase}\\data\\${filename}.json`, response, { spaces: 2 });
+        });
+
+}
+
+try {
+    go(__dirname + "\\images\\");
+} catch (error) {
+    console.log(error)
+}
+
+
